bae3dd958908c7e868c6fcfa35b7907b54dcbcc2
LucyBai <bai.lu2@husky.neu.edu>
2017-12-04 16:38:02 -0500

    fix

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CartComponent } from './cart/cart.component';
-import { ViewProductComponent } from './view-product/view-product.component';
 import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
@@ -12,7 +10,6 @@ import { DetailComponent } from './detail/detail.component';
     AppComponent,
     HeaderComponent,
     CartComponent,
-    ViewProductComponent,
     DetailComponent
   ],
   imports: [
