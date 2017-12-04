820e6ba238af190db644d0209d595e19bef3e564
YangyangYi <yi.yan@husky.neu.edu>
2017-12-03 19:54:50 -0500

    With login page

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule
