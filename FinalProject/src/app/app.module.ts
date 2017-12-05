518040d5c45c179b875bfb310679c0304bc1a651
YangyangYi <yi.yan@husky.neu.edu>
2017-12-05 18:33:00 -0500

    Ivy commit with home component

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponentComponent
+    LoginComponentComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ FinalProject/src/app/home-component/home-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-component',
+  templateUrl: './home-component.component.html',
+  styleUrls: ['./home-component.component.scss']
+})
+export class HomeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
