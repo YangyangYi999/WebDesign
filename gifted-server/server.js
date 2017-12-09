6510fc7378bd3fb084560fdae5e007df9485fe6b
Abhinn Ankit <ankit.a@husky.neu.edu>
2017-12-09 03:33:46 -0500

    Added server

---
+++ gifted-server/server.js
@@ -0,0 +1,70 @@
+let express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    debug = require('debug')('node-rest:server'),
+    port = process.env.PORT || 3000,
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/giftedDB', { useMongoClient: true });
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+// Set Routes
+
+app.listen(port);
+app.on('error', onError);
+app.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
+
+console.log('Server started on: ' + port);
