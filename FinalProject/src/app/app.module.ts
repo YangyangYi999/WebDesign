e746835d92b939467d63c0b9564afc9890034173
LucyBai <bai.lu2@husky.neu.edu>
2017-12-08 22:43:24 -0500

    merged lu

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CartComponent } from './cart/cart.component';
@@ -12,6 +13,12 @@ import { ItemComponentComponent } from './item-component/item-component.componen
 import { OrdersComponentComponent } from './orders-component/orders-component.component';
 import { CheckoutComponentComponent } from './checkout-component/checkout-component.component';
 import { MyShopAppComponent } from './my-shop-app/my-shop-app.component';
+import { FilterComponent } from './filter/filter.component';
+import { PaymentComponent } from './payment/payment.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -25,10 +32,16 @@ import { MyShopAppComponent } from './my-shop-app/my-shop-app.component';
     ItemComponentComponent,
     OrdersComponentComponent,
     CheckoutComponentComponent,
-    MyShopAppComponent
+    MyShopAppComponent,
+    FilterComponent,
+    PaymentComponent,
+    PageNotFoundComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
