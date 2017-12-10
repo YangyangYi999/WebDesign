6d1df3113f2eaa4438b54bf5b64b46b0ab2f81d5
Abhinn Ankit <ankit.a@husky.neu.edu>
2017-12-10 17:54:00 -0500

    Completed products table

--- gifted-server/models/product.js
+++ gifted-server/models/product.js
@@ -49,37 +49,4 @@ productSchema.plugin(mongooseUniqueValidator);
 
 let ProductModel = mongoose.model('Product', productSchema);
 
-let date = moment("11-23-2017", "MM-DD-YYYY");
-
-let tShirt = new ProductModel({
-    name: "PHOTO PRINT T-SHIRT",
-    description: "Round neck T-shirt with short sleeves and photographic print on the chest.",
-    rating: 5,
-    addedTime: date,
-    type: "t-shirt",
-    itemList: {
-        size: {
-            s: 10,
-            m: 10,
-            l: 10,
-            xl: 10
-        },
-        color: {
-            name: "black",
-            url: "https://static.zara.net/photos///2017/I/0/2/p/0679/327/250/2/w/1920/0679327250_1_1_1.jpg?ts=1511359447436"
-        },
-        price: {
-            s: 25.90,
-            m: 25.90,
-            l: 25.90,
-            xl: 25.90
-        }
-    }
-});
-
-tShirt.save(function (err, data) {
-    if (err) console.log(err);
-    else console.log('Saved ', data );
-});
-
 module.exports = ProductModel;
