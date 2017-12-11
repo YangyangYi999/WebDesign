83a03cfbe7bee96a8ea00960afb927d037318370
Abhinn Ankit <ankit.a@husky.neu.edu>
2017-12-11 03:46:32 -0500

    Added signin and signup routes

--- gifted-server/routes/user.js
+++ gifted-server/routes/user.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const bCrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Gifted Server' });
+const User = require('../models/user');
+
+router.post('/', function (req, res, next) {
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: bCrypt.hashSync(req.body.password, 10),
+        email: req.body.email
+    });
+    user.save(function (err, result) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        res.status(201).json({
+            message: 'User Created',
+            obj: result
+        });
+    });
+});
+
+router.post('/signin', function (req, res, next) {
+    User.findOne({email: req.body.email}, function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if(!user) {
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            });
+        }
+        if (!bCrypt.compareSync(req.body.password, user.password)) {
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            });
+        }
+        const token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            userId: user._id
+        });
+    });
 });
 
 module.exports = router;
